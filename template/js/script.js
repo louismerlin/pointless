1365b83b4b499436613c6739ae17ae865c3fb2fb
Louis Merlin <louis.merlin@epfl.ch>
2015-10-07 23:33:00 +0200

    Undo and Redo working, changed buttons hoveer color

--- template/js/script.js
+++ template/js/script.js
@@ -7,7 +7,8 @@ var bgE = document.getElementById("background"),
   interfaceE = document.getElementById("interface"),
   interfaceC = document.getElementById("interface").getContext("2d"),
   selectedDot = null,
-  image = [];
+  image = [],
+  redoImage = [];
 
 //BACKGROUND
 
@@ -31,6 +32,10 @@ function getMousePos(canvas, evt) {
 interfaceE.addEventListener('mousemove', function(e) {
   //var mousePos = getMousePos(interfaceE, e);
   drawDots(e.layerX, e.layerY);
+  if (selectedDot != null) {
+    interfaceC.fillRect(selectedDot.x * 20 + 8, selectedDot.y * 20 + 8, 4, 4);
+    drawTempLine(e.layerX, e.layerY);
+  }
 }, false);
 
 interfaceE.addEventListener('mousedown', function(e) {
@@ -38,17 +43,7 @@ interfaceE.addEventListener('mousedown', function(e) {
     x: Math.floor(e.layerX / 20),
     y: Math.floor(e.layerY / 20),
   }
-  tdC.clearRect(0,0,640,640);
-  /*if (selectedDot != null) {
-    if (tempDot.x == selectedDot.x && tempDot.y == selectedDot.y) {
-      selectedDot = null;
-      return;
-    }
-    else {
-      drawLine(tempDot);
-      selectedDot = tempDot;
-    }
-  }*/
+  tdC.clearRect(0, 0, 640, 640);
   selectedDot = tempDot;
 }, false);
 
@@ -57,19 +52,7 @@ interfaceE.addEventListener('mouseup', function(e) {
     x: Math.floor(e.layerX / 20),
     y: Math.floor(e.layerY / 20),
   }
-  /*tdC.clearRect(0,0,640,640);
-  if (selectedDot != null) {
-    if (tempDot.x == selectedDot.x && tempDot.y == selectedDot.y) {
-      selectedDot = null;
-      return;
-    }
-    else {
-      drawLine(tempDot);
-      selectedDot = tempDot;
-    }
-  }
-  selectedDot = tempDot;*/
-  drawLine(tempDot);
+  addLine(tempDot);
   selectedDot = null;
 }, false);
 
@@ -81,10 +64,6 @@ drawDots = function(x, y) {
     }
   }
   interfaceC.fillRect(Math.floor(x / 20) * 20 + 8, Math.floor(y / 20) * 20 + 8, 4, 4);
-  if (selectedDot != null) {
-    interfaceC.fillRect(selectedDot.x * 20 + 8, selectedDot.y * 20 + 8, 4, 4);
-    drawTempLine(x, y);
-  }
 }
 
 drawDots(-1, -1);
@@ -104,13 +83,26 @@ drawTempLine = function(x, y) {
 //DRAWING
 
 drawingC.strokeStyle = "#3B3228";
-drawLine = function(dot) {
-  drawingC.beginPath();
-  drawingC.moveTo(selectedDot.x * 20 + 10, selectedDot.y * 20 + 10);
-  drawingC.lineTo(dot.x * 20 + 10, dot.y * 20 + 10);
-  drawingC.stroke();
-  tdC.clearRect(0,0,640,640);
-  image.push({"x1": selectedDot.x, "y1": selectedDot.y, "x2": dot.x, "y2":dot.y});
+addLine = function(dot) {
+  image.push({
+    "x1": selectedDot.x,
+    "y1": selectedDot.y,
+    "x2": dot.x,
+    "y2": dot.y
+  });
+  redoImage = [];
+  drawImage(image);
+}
+
+drawImage = function(im) {
+  drawingC.clearRect(0, 0, 640, 640);
+  for (var i = 0; i < im.length; i++) {
+    drawingC.beginPath();
+    drawingC.moveTo(im[i].x1 * 20 + 10, im[i].y1 * 20 + 10);
+    drawingC.lineTo(im[i].x2 * 20 + 10, im[i].y2 * 20 + 10);
+    drawingC.stroke();
+  }
+  tdC.clearRect(0, 0, 640, 640);
 }
 
 importImage = function(im) {
@@ -119,5 +111,13 @@ importImage = function(im) {
 importImage(imageLine);
 
 undo = function() {
-  image.pop();
+  redoImage.push(image.pop());
+  drawImage(image);
+}
+
+redo = function() {
+  if (redoImage.length!=0) {
+    image.push(redoImage.pop());
+    drawImage(image);
+  }
 }
